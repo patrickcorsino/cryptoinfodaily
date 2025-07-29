59ba795b5762290d8f7395a283361c6f72d59e7c
patrickcorsino <patricklcorsino@gmail.com>
2025-07-29 13:38:29 -0500

    Delete components directory

--- components/Hero.js
+++
@@ -1,8 +0,0 @@
-export default function Hero() {
-  return (
-    <div className="text-center mb-10">
-      <h1 className="text-4xl font-bold">Welcome to CryptoInfoDaily</h1>
-      <p className="text-lg text-gray-300 mt-2">Your gamified crypto dashboard</p>
-    </div>
-  )
-}
