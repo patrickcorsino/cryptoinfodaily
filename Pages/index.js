368f6405fc8cf1b3e659c9d37a539e5aafc68e19
patrickcorsino <patricklcorsino@gmail.com>
2025-07-29 17:07:05 -0500

    Update index.js

--- Pages/index.js
+++ Pages/index.js
@@ -1,22 +1,28 @@
-import Head from 'next/head'
-import Hero from '../components/Hero'
-import CoinTable from '../components/CoinTable'
-import FearGreed from '../components/FearGreed'
+// pages/index.js
+import Head from 'next/head';
+import Hero from '@/components/Hero';
+import CoinTable from '@/components/CoinTable';
+import Navbar from '@/components/Navbar';
+import { getTrendingCoins } from '@/utils/api';
 
-export default function Home() {
+export async function getServerSideProps() {
+  const coins = await getTrendingCoins();
+  return { props: { coins } };
+}
+
+export default function Home({ coins }) {
   return (
-    <div>
+    <>
       <Head>
         <title>CryptoInfoDaily</title>
         <meta name="description" content="Stay updated. Trade smart. Have fun." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <Navbar coins={coins} />
       <main>
         <Hero />
-        <FearGreed />
-        <CoinTable />
+        <CoinTable coins={coins} />
       </main>
-    </div>
-  )
+    </>
+  );
 }
