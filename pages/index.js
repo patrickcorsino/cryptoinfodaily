d598807fb83cf5f5c6b7620dfb9132127311ff35
patrickcorsino <patricklcorsino@gmail.com>
2025-07-29 12:52:07 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to CryptoInfoDaily!</h1>
+    </div>
+  );
+}
 // Finalized UI index.js content
