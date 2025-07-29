61febe88c2fad03cf3d58f1a48a8ca9c689dae9e
patrickcorsino <patricklcorsino@gmail.com>
2025-07-29 12:47:49 -0500

    Delete Utils directory

--- Utils/fetchdata.js
+++
@@ -1,5 +0,0 @@
-export async function fetchTopCoins() {
-  const response = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false');
-  const data = await response.json();
-  return data;
-}
