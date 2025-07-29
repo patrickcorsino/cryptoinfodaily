a13de8f27972fe8bb108f631767783df9ad5fcc2
patrickcorsino <patricklcorsino@gmail.com>
2025-07-29 16:49:27 -0500

    Update index.js

--- Pages/index.js
+++ Pages/index.js
@@ -1,33 +1,9 @@
-import Head from 'next/head';
-import Hero from '@/Components/Hero';
-import CoinTable from '@/Components/CoinTable';
-import { useEffect, useState } from 'react';
+import Head from 'next/head'
+import Hero from '../components/Hero'
+import CoinTable from '../components/CoinTable'
 import FearGreed from '../components/FearGreed'
 
-
 export default function Home() {
-  const [coins, setCoins] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  const fetchData = async () => {
-    try {
-      const res = await fetch(
-        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=true'
-      );
-      const data = await res.json();
-      setCoins(data);
-      setLoading(false);
-    } catch (err) {
-      console.error('Error fetching coins:', err);
-    }
-  };
-
-  useEffect(() => {
-    fetchData(); // initial load
-    const interval = setInterval(fetchData, 30000); // every 30s
-    return () => clearInterval(interval); // cleanup
-  }, []);
-
   return (
     <div>
       <Head>
@@ -35,10 +11,12 @@ export default function Home() {
         <meta name="description" content="Stay updated. Trade smart. Have fun." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <main>
         <Hero />
-        {loading ? <p style={{ textAlign: 'center' }}>Loading coins...</p> : <CoinTable coins={coins} />}
+        <FearGreed />
+        <CoinTable />
       </main>
     </div>
-  );
+  )
 }
