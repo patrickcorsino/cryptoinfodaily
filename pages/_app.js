4ed31b54ba9d9fd930732f3282ccaefc84907536
patrickcorsino <patricklcorsino@gmail.com>
2025-07-29 12:25:56 -0500

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,5 @@
+import '@/styles/globals.css'; // Adjust this path only if your globals.css is elsewhere
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
