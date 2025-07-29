1f6e8829a224e8cb2741113328ed193fbd98ef4f
patrickcorsino <patricklcorsino@gmail.com>
2025-07-29 10:58:53 -0500

    Update SearchModal.js

--- Components/SearchModal.js
+++ Components/SearchModal.js
@@ -8,7 +8,7 @@ export default function SearchModal({ open, onClose }) {
   useEffect(() => {
     if (!query) return setResults([]);
     const timer = setTimeout(async () => {
-      const res = await fetch(\`https://api.coingecko.com/api/v3/search?query=\${query}\`);
+      const res = await fetch(`https://api.coingecko.com/api/v3/search?query=${query}`);
       const data = await res.json();
       setResults(data.coins || []);
     }, 300);
