8e37b5daf397cb9fc0aeeaf1f0ae48ca529a9e21
patrickcorsino <patricklcorsino@gmail.com>
2025-07-29 16:28:17 -0500

    Update index.js

--- Pages/index.js
+++ Pages/index.js
@@ -1,1 +1,19 @@
-//index.js
+import Head from 'next/head';
+import Hero from '@/Components/Hero';
+import CoinTable from '@/Components/CoinTable';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>CryptoInfoDaily</title>
+        <meta name="description" content="Stay updated. Trade smart. Have fun." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="bg-gradient-to-b from-[#0f172a] to-[#1e293b] min-h-screen text-white">
+        <Hero />
+        <CoinTable />
+      </main>
+    </>
+  );
+}
